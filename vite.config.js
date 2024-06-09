a9da512044e9bf8d48c2b6101bc9b44ea3dd6e19
M.A. Asardeen <asardeenbse@gmail.com>
2024-06-09 21:29:28 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-react-router/",
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
   },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
-})
+});
